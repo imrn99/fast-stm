370d96857af9a91bc25118eb3a1b5de562081845
imrn99 <imrn99@users.noreply.github.com>
2024-11-03 12:38:14 +0000

    deploy: e190ee9ae2d9b98792d12a384ddbf3f5b2b3266a

---
+++ trait.impl/core/clone/trait.Clone.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["fast_stm",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"fast_stm/enum.StmError.html\" title=\"enum fast_stm::StmError\">StmError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"enum\" href=\"fast_stm/enum.TransactionControl.html\" title=\"enum fast_stm::TransactionControl\">TransactionControl</a>"],["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"fast_stm/struct.TVar.html\" title=\"struct fast_stm::TVar\">TVar</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[942]}
