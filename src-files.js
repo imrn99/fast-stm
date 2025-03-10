6d260c49f6a349e7c3aea795d1f43cb347148ea1
imrn99 <imrn99@users.noreply.github.com>
2025-03-10 09:25:25 +0000

    deploy: f3f80c4ec0330059f778b14e75c090f22c33e294

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["benches",["",[],["lib.rs"]]],["fast_stm",["",[["transaction",[],["control_block.rs","log_var.rs","mod.rs"]]],["lib.rs","result.rs","tvar.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[30,115]}
---
+++ trait.impl/core/marker/trait.StructuralPartialEq.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["fast_stm",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"fast_stm/enum.StmError.html\" title=\"enum fast_stm::StmError\">StmError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"fast_stm/enum.TransactionControl.html\" title=\"enum fast_stm::TransactionControl\">TransactionControl</a>"],["impl&lt;E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"fast_stm/enum.TransactionError.html\" title=\"enum fast_stm::TransactionError\">TransactionError</a>&lt;E&gt;"],["impl&lt;T, E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"fast_stm/enum.TransactionResult.html\" title=\"enum fast_stm::TransactionResult\">TransactionResult</a>&lt;T, E&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1313]}
