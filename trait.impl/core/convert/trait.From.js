2eb5fe81cc74081fdc5f0b37f1476e0658841364
imrn99 <imrn99@users.noreply.github.com>
2025-12-02 07:26:31 +0000

    deploy: 49a6de2d3d90029dc1ca61ef4a47e8193ae4bdfc

---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["fast_stm",[["impl&lt;E&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"fast_stm/enum.StmError.html\" title=\"enum fast_stm::StmError\">StmError</a>&gt; for <a class=\"enum\" href=\"fast_stm/enum.TransactionError.html\" title=\"enum fast_stm::TransactionError\">TransactionError</a>&lt;E&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[416]}
