577b9c589223169bf722fb480c685e1bb374124f
imrn99 <95699343+imrn99@users.noreply.github.com>
2025-12-19 08:57:59 +0100

    refactor: replace `shell.nix` file with flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, flake-utils, fenix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        
+        commonBuildInputs = with pkgs; [
+          # Rust
+          (fenix.packages.${system}.stable.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+
+          # Tools
+          cargo-nextest
+          samply
+          taplo
+        ];
+        linuxBuildInputs = with pkgs; [
+        ];
+        darwinBuildInputs = with pkgs; [
+          libiconv
+        ];
+
+        buildInputs = commonBuildInputs
+          ++ (if pkgs.stdenv.isLinux  then linuxBuildInputs  else [])
+          ++ (if pkgs.stdenv.isDarwin then darwinBuildInputs else []);
+
+        ldLibraryPath = pkgs.lib.makeLibraryPath ( buildInputs );
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = buildInputs;
+          
+          LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${ldLibraryPath}";
+        };
+      });
+}
