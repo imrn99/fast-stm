8bd19bc7d99908a290d6ace7a76dc4cdd584f069
imrn99 <95699343+imrn99@users.noreply.github.com>
2026-01-20 14:24:30 +0100

    feat(benches): add "n-th transactional access" benchmarks

--- benches/benches/write.rs
+++ benches/benches/write.rs
@@ -1,20 +1,37 @@
 use std::hint::black_box;
 use std::sync::atomic::{AtomicU32, Ordering};
 
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{
+    criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration,
+    Throughput,
+};
 use fast_stm::{atomically, init_transaction, TVar};
 
 /// Write routines benchmarks
 ///
+/// # Group 1
+///
 /// Compare:
+///
 /// - `TVar::write` in transaction
 /// - `TVar::write` alone
 /// - `TVar::write_atomic`
 /// - `Atomic::store`
+///
+/// execution times.
+///
+/// # Group 2
+///
+/// Execution times for N writes of different variables.
+///
+/// # Group 3
+///
+/// Execution time of the N-th write of a transaction.
 pub fn criterion_benchmark(c: &mut Criterion) {
     let old_value = 42_u32;
     let new_value = 12345;
 
+    // G1
     let mut g1 = c.benchmark_group("write-times");
 
     g1.bench_function("TVar::<u32>::write (alone)", |b| {
@@ -38,11 +55,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     g1.finish();
 
+    // G2
     let n_writes = [1_000, 10_000, 100_000, 1_000_000];
     let tvars: Vec<_> = (0..*n_writes.last().unwrap())
         .map(|_| TVar::new(old_value))
         .collect();
+
     let mut g2 = c.benchmark_group("write-times-vs-n-writes");
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+    g2.plot_config(plot_config);
+
     for n_write in n_writes {
         g2.throughput(Throughput::Elements(n_write));
         g2.bench_with_input(
@@ -60,6 +82,27 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         );
     }
     g2.finish();
+
+    // G3
+    let mut g3 = c.benchmark_group("nth-write-times");
+    let plot_config = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+    g3.plot_config(plot_config);
+
+    for n_write in n_writes {
+        g3.bench_with_input(
+            BenchmarkId::new("TVar::<u32>::write", n_write),
+            &(n_write, &tvars),
+            |b, &(n, tvs)| {
+                let mut tx = init_transaction();
+                // write n-1 variables, then benchmark the next write
+                for i in 0..n - 1 {
+                    let _ = tx.write(&tvs[i as usize], new_value);
+                }
+                b.iter(|| tx.write(&tvs[n as usize - 1], new_value))
+            },
+        );
+    }
+    g3.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
