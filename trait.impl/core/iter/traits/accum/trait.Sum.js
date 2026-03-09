4e75913ef8637d320289b232561e9a7aa7bcf095
imrn99 <imrn99@users.noreply.github.com>
2026-03-09 09:54:00 +0000

    deploy: aeb6e3ae9d3cb97f2a5233dde71bc5a564d93c86

---
+++ trait.impl/core/iter/traits/accum/trait.Sum.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["fast_stm",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/accum/trait.Sum.html\" title=\"trait core::iter::traits::accum::Sum\">Sum</a> for <a class=\"struct\" href=\"fast_stm/struct.TransactionTallies.html\" title=\"struct fast_stm::TransactionTallies\">TransactionTallies</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[322]}
