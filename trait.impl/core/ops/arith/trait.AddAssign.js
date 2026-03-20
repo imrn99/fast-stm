c3f32988a5253a66a26bd17876abbb056d582555
imrn99 <imrn99@users.noreply.github.com>
2026-03-20 13:12:01 +0000

    deploy: f640d8f7aa75a19c0ca7af79675696619dc513d7

---
+++ trait.impl/core/ops/arith/trait.AddAssign.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["fast_stm",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a> for <a class=\"struct\" href=\"fast_stm/struct.TransactionTallies.html\" title=\"struct fast_stm::TransactionTallies\">TransactionTallies</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[323]}
